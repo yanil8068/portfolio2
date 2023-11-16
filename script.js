9a73a0f0a446a94520d38cea81c413845d7cf411
yanil8068 <yanil8068@gmail.com>
2023-11-16 19:39:25 +0530

    added the nav bar for responsive page

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var menubtn = document.getElementById("menubtn");
+var navigation = document.getElementById("navigation");
+var menu = document.getElementById("menu");
+
+//   very important javascript
+navigation.style.right = "-200px";
+menubtn.onclick = function () {
+  if (navigation.style.right == "-200px") {
+    navigation.style.right = "0px";
+    menu.src = "images/close.png";
+  } else {
+    navigation.style.right = "-200px";
+    menu.src = "images/menu.png";
+  }
+};
+
+
 
